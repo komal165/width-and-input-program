325d9a933d625e5545c115163052f2e708c40363
komal <komal@LAPTOP-GPSG7DIK>
2019-05-27 00:35:18 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace width_and_input_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int x;
+
+                Console.Write("Enter a number: ");
+                x = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("{0}{0}{0}", x);
+                Console.WriteLine("{0} {0}", x);
+                Console.WriteLine("{0} {0}", x);
+                Console.WriteLine("{0} {0}", x);
+                Console.WriteLine("{0}{0}{0}", x);
+            }
+        }
+
+    }
+}
